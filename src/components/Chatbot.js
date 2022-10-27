f85de5a4e9303d9a9dfb0d243610936a280beb68
Joel <joeljhojan9@gmail.com>
2022-10-27 05:41:05 -0500

    otro c

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,10 +1,10 @@
-import bot from '../assets/img/bot-conversacional.png'
+
 import {useState,useEffect} from "react";
 import Axios from "axios"
 import generalContext from '../contex/generalContext';
 import { useContext } from 'react';
-import { Card } from "./componentesChatbot/card.js"
-
+import Card from "./componentesChatbot/Card";
+import Mensajes from './componentesChatbot/Mensajes';
 
 
 const Chatbot = () => {
@@ -19,34 +19,6 @@ const {chatCompleto , agregarMensaje} = generalsContext;
 
 const [mensaje , guardarMensaje ] = useState({texto:''})
 
-
-//Abrir el espacio de widgets
-const [openWidget, setopenWidget] = useState(false);
-
-const [cards, setCards] = useState([]);
-
-const eventOpenWidget = () => {
-  if (openWidget) {
-    setopenWidget(false);
-  } else {
-    setopenWidget(true);
-  }
-};
-
-const eventAddCard = (url,id,title,ubicacion)=>{
-  const newCards = [...cards];
-  newCards.push({
-    url: url,
-    id: id,
-    title: title,
-    ubicacion: ubicacion
-  });
-  setCards(newCards);
-}
-const eventDeletCards = ()=>{
-  const newCards = [];
-  setCards(newCards);
-}
 
 const onchangeMensaje = (e) => {
   guardarMensaje({
@@ -66,6 +38,8 @@ if (mensaje.texto === '') {
 guardarMensaje(mensaje)
 
 textQuery(mensaje.texto)
+
+guardarMensaje({texto:''})
 
 }
 
@@ -161,7 +135,7 @@ agregarMensaje(conversation)
 
       }
   
-
+    
       
 
   return (
@@ -173,75 +147,31 @@ agregarMensaje(conversation)
           </div>
         </header>
 
-        <section className={`bg-white h-full overflow-y-scroll no-scrollbar`}>
+        <section className={` bg-white h-full overflow-y-scroll no-scrollbar`}>
           
-          <ul className="grid p-4 gap-4">
+          <ul className="relative grid p-4 gap-4" >
  {
       
       chatCompleto.map((chat) => (
-      
-        chat.who === 'user' ? (
-           
-            <li key={chat.id} className="max-w-[60%] justify-self-end p-2 bg-coloru rounded-l-2xl rounded-tr-2xl ">
-              <span className="flex text-left text-sm px-2 text-colorb flex-nowrap">{chat.content.text.text}</span>
-            </li>
-           
-        ):(
 
-            <li key={chat.id} className="flex max-w-[60%]  justify-self-start p-2 shadow-sm bg-colorm gap-4 rounded-t-2xl rounded-br-2xl break-words">
-              <div className="bg-red-500 p-2 rounded-full border-4 border-red-200 ">
-                <img className="w-8 object-cover" src={bot} alt="bot" />
-              </div>
-              <div className='text-sm pr-2 text-colorb'>{chat.content.text.text}</div>
-            </li>
-
-          )
+        chat.content.card ?
+        ( <Card
+          key={chat.id}
+          card={chat}/>
+        
+        )
+         :(
+         <Mensajes
+          key={chat.id}
+          chat={chat}/>
+         )
+       
         )
       )
-    }
 
+ }
           </ul>
 
-
-        </section>
-
-            <section
-              className={`${!openWidget && "h-0"} ${
-                openWidget && "h-4/5"
-              } transition-all text-center`}
-            >
-              <button
-                className={`border-none text-2xl text-colorwc p-0 ${
-                  !openWidget && "hidden"
-                } ${openWidget && "inline"}`}
-                onClick={eventOpenWidget && eventDeletCards}
-              >
-                <i className={`fa-solid fa-x`}></i>
-              </button>
-              <div
-                className={`w-full h-full p-4 flex gap-4 border-2 border-t-colorwc border-l-colorwc border-r-colorwc border-b-colorb overflow-y-hidden overflow-x-scroll no-scrollbar`}
-              >
-              {
-                cards.map((card)=>(
-                  <Card
-                    url={card.url}
-                    id={card.id}
-                    title={card.title}
-                    ubicacion={card.ubicacion}
-                  ></Card>
-                ))
-              }
-              </div>
-            </section>
-      
-        <section
-              className={`bg-colorb w-full p-2 flex-nowrap overflow-x-scroll overflow-y-hidden no-scrollbar ${
-                !openWidget && "flex"
-              } ${openWidget && "hidden"}`}
-            >
-              <span className="h-10 min-w-[20%] max-w-[50%] bg-colorwc flex  items-center overflow-hidden whitespace-nowrap text-ellipsis  px-2 py-3 rounded-2xl text-grey-900 text-sm cursor-pointer hover:bg-colorwo hover:text-colorb">
-                <p>Muestame los lugares turisticos</p>
-              </span>
         </section>
 
         <footer className=" rounded-b-2xl bg-white p-2">
--- src/components/componentesChatbot/card.js
+++ src/components/componentesChatbot/card.js
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { Fragment , useState } from "react";
+
 
 function Card(props){
+  
+  const {card} = props;
+
+  const [modal , switchModal] = useState(false);
+
+  const vModal = modal ? "absolute  w-full h-full z-50 bg-colorwo p-4" : "hidden";
+
+  const toggleModal = () => {
+    if (modal) {
+      switchModal(false)
+    } else {
+      switchModal(true)
+    
+    }
+    
+  }
+
+  
+
+
+
   return(
-      <div key={props.id} className="h-[90%] max-w-[60%] bg-colorwo rounded-2xl grid grid-rows-2 ">
-        <span className="w-full h-1/4"><img className="w-full rounded-t-2xl object-cover"></img> </span>
+    <Fragment>
+
+    
+      <div key={card.id} className=" max-w-[90%] h-80 z-20 bg-colorwo rounded-2xl grid grid-rows-2 ">
+        <span className="w-full h-1/4"><img src={card.content.card.imageUri} alt='imagen no disponible' className="w-full rounded-t-2xl object-cover"></img> </span>
           <div className="flex flex-col justify-between text-colorb">
-            <div className="px-4 py-2 gap-2">
-              <p className="text-left font-bold text-sm">Kotosh, Templo de las manos cruzadas</p>
-              <p className="text-left font-normal text-xs">A 5 kilómetros de la ciudad de Huánuco</p>
+            <div className="px-3 pt-20 gap-2">
+              <p className="text-left font-bold text-sm">{card.content.card.title}</p>
+              <p className="text-left font-normal text-xs">{card.content.card.buttons[0].postback}</p>
             </div>
-            <button className="w-full h-8 rounded-b-2xl font-medium hover:bg-colorwc hover:text-colorwo">Seleccionar</button>
+            <button className="w-full h-8 rounded-b-2xl font-medium hover:bg-colorwc hover:text-colorwo" onClick={toggleModal}>Seleccionar</button>
           </div>
       </div>
+
+      <div className={vModal}>
+      <span  className="final w-full h-1/4"><img src={card.content.card.imageUri} alt='imagen no disponible' className="w-full rounded-2xl object-cover"></img> </span>
+         <p className="text-colorwc text-center py-4 font-bold">{card.content.card.title}</p>
+         <p className="text-justify text-colorwc">{card.content.card.subtitle}</p>
+
+         <p className="text-colorwc text-center py-4 font-bold">Ubicación</p>
+         <p className="text-justify text-colorwc">{card.content.card.buttons[0].text}</p>
+
+         <p></p>
+
+       <button className="py-4  w-full mt-4 bg-colorp opacity-40 rounded-lg justify-center" onClick={toggleModal}>Salir</button>
+      </div>
+      
+      </Fragment>
   );
 }
 
-export { Card };
+export default Card;
